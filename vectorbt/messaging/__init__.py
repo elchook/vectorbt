c45a63b927c91304205bd2f0c07dcc1106570dd3
Chook <110505523+elchook@users.noreply.github.com>
2023-02-07 11:05:15 +1000

    Update __init__.py

--- vectorbt/messaging/__init__.py
+++ vectorbt/messaging/__init__.py
@@ -6,13 +6,15 @@
 __all__ = []
 __blacklist__ = []
 
-try:
-    import telegram
-except ImportError:
-    __blacklist__.append('telegram')
-else:
-    from vectorbt.messaging.telegram import TelegramBot
+__blacklist__.append('telegram')
 
-    __all__.append('TelegramBot')
+# try:
+#     import telegram
+# except ImportError:
+#     __blacklist__.append('telegram')
+# else:
+#     from vectorbt.messaging.telegram import TelegramBot
+
+#     __all__.append('TelegramBot')
 
 __pdoc__ = {k: False for k in __all__}
